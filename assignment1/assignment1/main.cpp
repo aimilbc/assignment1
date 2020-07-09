c26c05e0a3ee2327c07244e13197191ffe55b721
aimilbc <touyamaaimi927@gmail.com>
2020-07-08 23:31:02 -0700

    initial commit
    creating a new file

---
+++ assignment1/assignment1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  assignment1
+//
+//  Created by Aimi Ross on 7/8/20.
+//  Copyright © 2020 Aimi Ross. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
